208dec7d550c97b3e9d257769c275597054c42a0
Wafl97 <mlwellerb@gmail.com>
2020-11-03 10:30:07 +0100

    Update Item.java
    Item class done

--- src/main/java/dk/sdu/mmmi/t3/g1/Item.java
+++ src/main/java/dk/sdu/mmmi/t3/g1/Item.java
@@ -3,4 +3,17 @@ package dk.sdu.mmmi.t3.g1;
 public class Item {
     private String name;
     private String type;
+
+    public Item(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getType(){
+        return type;
+    }
 }
