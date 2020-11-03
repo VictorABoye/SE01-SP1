216e127e5affb05a198be8b8e8381a9c47e8e7ef
ZenOfWolf <zenofwolf@live.dk>
2020-11-03 10:44:44 +0100

    Added methods for Inventory class

--- src/main/java/dk/sdu/mmmi/t3/g1/Inventory.java
+++ src/main/java/dk/sdu/mmmi/t3/g1/Inventory.java
@@ -4,4 +4,19 @@ import java.util.ArrayList;
 
 public class Inventory {
     private ArrayList<Item> items;
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    public void showInventory(){
+        System.out.println("Inventory:");
+        for(Item item: items){
+            System.out.println(item.getName());
+        }
+    }
 }
