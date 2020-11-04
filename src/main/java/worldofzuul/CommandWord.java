327f5787d8c98c6f4ea3cad9dafdd10ee1ae119c
Victor <zenofwolf@live.dk>
2020-11-04 13:37:52 +0100

    Fixed comma....

--- src/main/java/worldofzuul/CommandWord.java
+++ src/main/java/worldofzuul/CommandWord.java
@@ -4,7 +4,7 @@ public enum CommandWord
 {
     GO("go"), QUIT("quit"), HELP("help"), SORT("sort"), PICKUP("take"),
     INVENTORY("bag"), PLACE("drop"), SCORE("score"), CHOOSE("choose"),
-    ROOMINVENTORY("room") UNKNOWN("?");
+    ROOMINVENTORY("room"), UNKNOWN("?");
     
     private String commandString;
     
