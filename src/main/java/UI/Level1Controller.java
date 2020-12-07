21bbf1a76eca0dc99137edf3adccdb1536103e42
Victor <zenofwolf@live.dk>
2020-12-07 15:51:02 +0100

    Temp popUp xD

--- src/main/java/UI/Level1Controller.java
+++ src/main/java/UI/Level1Controller.java
@@ -1,15 +1,23 @@
 package UI;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import worldofzuul.Game;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Level1Controller extends PlayerControl{
+
+    final String questFile = "/fxml/PopUp.fxml";
 
     @FXML
     public AnchorPane window;
@@ -31,5 +39,16 @@ public class Level1Controller extends PlayerControl{
         populateArraylist();
         window.getChildren().addAll(createScoreBar());
         setImages(itemsInRoom);
+        try{
+            Parent popUp = FXMLLoader.load(Level1Controller.class.getResource(questFile));
+            Stage stage = new Stage();
+            Scene scene = new Scene(popUp);
+            stage.setScene(scene);
+            stage.setAlwaysOnTop(true);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
