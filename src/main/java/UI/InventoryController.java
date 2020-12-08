a2c5e6593d80b322853f709aa1cff7ac1283a7f3
Wafl97 <mlwellerb@gmail.com>
2020-12-08 13:56:28 +0100

    Fixed InventoryController

--- src/main/java/UI/InventoryController.java
+++ src/main/java/UI/InventoryController.java
@@ -26,29 +26,41 @@ public class InventoryController {
     }
 
     public void render(){
-    Text text = (Text) window.lookup("#text");
-    Inventory playerInventory = Game.getWorldPlayer().getInventory();
+        Text text = (Text) window.lookup("#text");
+        Inventory playerInventory = Game.getWorldPlayer().getInventory();
         if (playerInventory.getSize() > 0)
-    {
-        for (int i = 0; i < playerInventory.getSize(); i++)
         {
-            String url = playerInventory.getItem(i).getImageView().getImage().getUrl();
-            ImageView itemView = new ImageView(new Image(url));
-            itemView.setFitHeight(size);
-            itemView.setFitWidth(size);
-            itemView.setLayoutY(text.getLayoutY()+padding);
-            itemView.setLayoutX((size*i+padding*i)+padding);
-            window.getChildren().add(itemView);
+            int viggo = 0;
+            for (int i = 0; i < playerInventory.getSize(); i++)
+            {
+                if (!playerInventory.getItem(i).isSorted()) {
+
+                    String url = playerInventory.getItem(i).getImageView().getImage().getUrl();
+                    ImageView itemView = new ImageView(new Image(url));
+                    itemView.setId(playerInventory.getItem(i).getImageView().getId());
+                    itemView.setFitHeight(size);
+                    itemView.setFitWidth(size);
+                    itemView.setLayoutY(text.getLayoutY() + padding);
+                    itemView.setLayoutX((size * viggo + padding * viggo) + padding);
+                    window.getChildren().add(itemView);
+                    viggo++;
+                }else{
+                    String url = playerInventory.getItem(i).getImageView().getImage().getUrl();
+                    ImageView itemView = new ImageView(new Image(url));
+                    itemView.setId(playerInventory.getItem(i).getImageView().getId());
+                    itemView.setLayoutX(padding);
+
+                }
+            }
         }
-    }
         else
-    {
-        Text label = new Text();
-        label.setText("Empty");
-        label.setLayoutX(text.getLayoutX());
-        label.setLayoutY(text.getLayoutY()+text.getFont().getSize()+padding);
-        label.setFont(text.getFont());
-        window.getChildren().add(label);
-    }
+        {
+            Text label = new Text();
+            label.setText("Empty");
+            label.setLayoutX(text.getLayoutX());
+            label.setLayoutY(text.getLayoutY()+text.getFont().getSize()+padding);
+            label.setFont(text.getFont());
+            window.getChildren().add(label);
+        }
 }
 }
